c76f2b499c462044a37cda802d88c26e659821f4
usehun <biny1220@gmail.com>
2022-12-06 23:07:01 +0900

    add image blending

--- js/main.js
+++ js/main.js
@@ -113,7 +113,7 @@
       values: {
         rect1X: [0, 0, { start: 0, end: 0 }],
         rect2X: [0, 0, { start: 0, end: 0 }],
-        imageBlendY: [0, 0, { start: 0, end: 0 }],
+        blendHeight: [0, 0, { start: 0, end: 0 }],
         rectStartY: 0,
       },
     },
@@ -444,11 +444,9 @@
           if (widthRatio <= heightRatio) {
             // 캔버스보다 브라우저 창이 홀쭉한 경우
             canvasScaleRatio = heightRatio;
-            // console.log("heightRatio로 결정");
           } else {
             // 캔버스보다 브라우저 창이 납작한 경우
             canvasScaleRatio = widthRatio;
-            // console.log("widthRatio로 결정");
           }
 
           objs.canvas.style.transform = `scale(${canvasScaleRatio})`;
@@ -496,11 +494,11 @@
         if (widthRatio <= heightRatio) {
           // 캔버스보다 브라우저 창이 홀쭉한 경우
           canvasScaleRatio = heightRatio;
-          console.log("heightRatio로 결정");
+          // console.log("heightRatio로 결정");
         } else {
           // 캔버스보다 브라우저 창이 납작한 경우
           canvasScaleRatio = widthRatio;
-          console.log("widthRatio로 결정");
+          // console.log("widthRatio로 결정");
         }
 
         objs.canvas.style.transform = `scale(${canvasScaleRatio})`;
@@ -568,7 +566,24 @@
           step = 2;
           // console.log("캔버스 닿기 후")
           // 이미지 블렌드
-          // objs.context.drawImage(img, x, y, width, height);
+          values.blendHeight[0] = 0;
+          values.blendHeight[1] = objs.canvas.height;
+          values.blendHeight[2].start = values.rect1X[2].end;
+          values.blendHeight[2].end = values.blendHeight[2].start + 0.2;
+          const blendHeight = calcValues(values.blendHeight, currentYOffset);
+
+          objs.context.drawImage(
+            objs.images[1],
+            0,
+            objs.canvas.height - blendHeight,
+            objs.canvas.width,
+            blendHeight,
+            0,
+            objs.canvas.height - blendHeight,
+            objs.canvas.width,
+            blendHeight
+          );
+
           objs.canvas.classList.add("sticky");
           objs.canvas.style.top = `${
             -(objs.canvas.height - objs.canvas.height * canvasScaleRatio) / 2
